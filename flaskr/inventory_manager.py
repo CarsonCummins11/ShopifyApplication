785ac5d287b18b17fc83bff47d13d3c88e409b1b
Carson Cummins <carsoncummins@Carsons-MacBook-Pro.local>
2022-01-08 01:15:11 -0800

    commented all the backend python

--- flaskr/inventory_manager.py
+++ flaskr/inventory_manager.py
@@ -15,7 +15,8 @@ class Manager:
 
     '''
     makes use of a classic mongodb design pattern, although normally this will include a sort command for relevance to a search
-    query - we obviously don't have that as we don't have a search query
+    query - we obviously don't have that as we don't have a search query.
+    Pagination will help decrease load times for our site, making it much more user friendly
     '''
     def get_page(self,page):
         return list(
@@ -42,23 +43,28 @@ class Manager:
                 item.to_dict()
             )
         return self.next_id - 1
-    '''
     
+    '''
+    essentially replaces the old item with the new one in our database using the mongodb update command.
+    Doing this instead of delete, insert allows us to speed up db access by using only one DB op rather than 2
     '''
     def edit_item(self, id,item):
         self.db['inventory'].update_one(
             {'id_':id},
             {'$set':item.to_dict()}
         )
+
     '''
-    
+    just a wrapper for a standard mongodb command
     '''
     def get_item(self,id):
         return self.db['inventory'].find_one({'id_':id})
     
 
     '''
-    
+    lists each item in our database and adds it to a python list object
+    keeping everything in memory allows us to stay speedy, but this could be converted to multiple hard disk ops
+    if the # of items becomes large enough
     '''
     def to_csv(self):
         csv_list = [['name','id','description','quantity','unit value','total value']]
--- flaskr/item.py
+++ flaskr/item.py
@@ -1,12 +1,23 @@
+'''
+simple data class to represent an item in the inventory system
+'''
+
 class Item():
+
+    '''
+    constructor with some defaults built in to keep everything clean
+    '''
     def __init__(self, id_ ,name='New Item', description='No Description',quantity=1,unit_value=0):
         self.id_ = id_
         self.name = name
         self.description = description
         self.quantity = quantity
         self.unit_value = unit_value
-        self.total_value = unit_value * quantity
+        self.total_value = unit_value * quantity #keep this value calculated, should never differ from quantity * unit_value (obviously)
 
+    '''
+    just dumps our object data into a python dictionary so this object can interface with mongodb easily
+    '''
     def to_dict(self):
         return {
             'id_':self.id_,
@@ -17,10 +28,20 @@ class Item():
             'unit_value': self.unit_value
         }
 
+
+    '''
+    allows us to get the object back from data stored in mongodb
+    by simply calling the constructor with the provided dictionary's values
+    '''
     @staticmethod
     def from_dict(data):
         return Item(data['id_'],data['name'],data['description'],data['quantity'],data['unit_value'])
 
+
+    '''
+    converts all of this objects fields to a list so we can output to a CSV
+    order is defined in the inventory_manager.py file
+    '''
     def to_list(self):
         return [
                 self.name,
@@ -30,6 +51,11 @@ class Item():
                 self.unit_value,
                 self.total_value
             ]
+
+    '''
+    setters for the unit value and quantity that should be used
+    in order to ensure total_value stays correct
+    '''
 
     def set_unit_value(self, value):
         self.unit_value = value
