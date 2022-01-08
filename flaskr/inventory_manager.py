df3896980682592cdf3a9811cc66d8d5c7c0cef0
Carson Cummins <carsoncummins@Carsons-MacBook-Pro.local>
2022-01-08 01:04:54 -0800

    some documentation

--- flaskr/inventory_manager.py
+++ flaskr/inventory_manager.py
@@ -2,22 +2,39 @@ from flaskr.item import Item
 
 class Manager:
 
-    PAGE_SIZE = 5
+    PAGE_SIZE = 5 #the number of items to render on a given page - keeping it low so it's easy to check out this functionality
 
+    '''
+    constructor to allow this object persistent access to the database
+    and to maintain track of what the next unique id will be (simply incrementing an int, assuming no more than 2^32 items will be created)
+    '''
     def __init__(self,database):
         self.db = database
         self.next_id = 0
     
+
+    '''
+    makes use of a classic mongodb design pattern, although normally this will include a sort command for relevance to a search
+    query - we obviously don't have that as we don't have a search query
+    '''
     def get_page(self,page):
         return list(
                 self.db['inventory'].find()
                 .skip(page * Manager.PAGE_SIZE)
                 .limit(Manager.PAGE_SIZE)
             )
-    
+    '''
+    just uses the mongodb built in delete method, very simple
+    '''
     def delete_item(self,id):
         self.db['inventory'].delete_one({'id_':id})
     
+
+    '''
+    creates an item using the item constructor to get the defaults in a clean pythonic way
+    increments the next_id to the next unique id
+    and puts the the new item in our database
+    '''
     def create_item(self):
         item = Item(self.next_id)
         self.next_id += 1
@@ -25,16 +42,24 @@ class Manager:
                 item.to_dict()
             )
         return self.next_id - 1
-
+    '''
+    
+    '''
     def edit_item(self, id,item):
         self.db['inventory'].update_one(
             {'id_':id},
             {'$set':item.to_dict()}
         )
+    '''
     
+    '''
     def get_item(self,id):
         return self.db['inventory'].find_one({'id_':id})
     
+
+    '''
+    
+    '''
     def to_csv(self):
         csv_list = [['name','id','description','quantity','unit value','total value']]
         items = self.db['inventory'].find()
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,30 +1,60 @@
+'''
+This does the main backend routing
+Should be imported from __init__.py to set up all of the routes for the site
+'''
+
 from flask import render_template, request, redirect, make_response
 from flaskr import app, manager
 from flaskr.item import Item
 import csv
 from io import StringIO
 
+
+'''
+Because we use pagination on the list of items, the {URL}/?page={A NUMBER} syntax is a little sloppy
+so instead we will force a redirect to the item_list route
+'''
 @app.route('/',methods=['GET'])
 def home():
     return redirect('/item_list')
 
+
+'''
+ask our database manager for the page passed in the URL arguments
+we'll then return the relevant pages
+actual db pagination functionality is in another place, keeping the routes file brief is very important
+'''
 @app.route('/item_list',methods=['GET'])
 def item_list():
     page = int('0' if request.args.get('page') == None else request.args.get('page'))
     items = manager.get_page(page)
     return render_template('home.html',items=items)
 
+'''
+asks the manager to delete an item with the id given in the URL arguments,
+again kept brief to minimize routes file length
+'''
 @app.route('/delete_item',methods=['GET'])
 def delete_item():
     id_ = int(request.args.get('id'))
     manager.delete_item(id_)
     return redirect('/')
 
+'''
+creates a new item with default fields
+then redirects to the edit page to allow the user to edit their new item
+'''
 @app.route('/create_item',methods=['GET'])
 def create_item():
     id_ = manager.create_item()
     return redirect('/edit_item?id='+str(id_))
 
+'''
+the CSV download functionality
+The simplest way to do this is just to write the csv file to a stringIO object, bypassing the need to access the disk,
+increasing speed by a large amount. Obviously, if massive amounts of inventory exceeding RAM need to be exported, this solution will need to change,
+however exporting quickly, to me at least, seems like a good goal.
+'''
 @app.route('/download',methods=['GET'])
 def download():
     csv_str = manager.to_csv()
@@ -36,6 +66,13 @@ def download():
     output.headers["Content-type"] = "text/csv"
     return output
 
+
+'''
+allows us to edit an item
+using a route allowing both get and post requests to minimize the number of unique routes to worry about
+The post request simply creates a new Item object based on the data in the form and uses the manager to update the relevant item
+the get request just finds the goal item and renders the HTML to edit it.
+'''
 @app.route('/edit_item',methods=['POST','GET'])
 def edit_item():
     if request.method == 'POST':
